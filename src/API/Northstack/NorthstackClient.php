27589a3ae5410b880a63acd183ca187e9dc9979d
Joshua Eichorn <joshua.eichorn@pagely.com>
2018-09-11 23:12:45 +0000

    Add a launch worker command, mark it as debug

---
+++ src/API/Northstack/NorthstackClient.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace NorthStack\NorthStackClient\API\Northstack;
+
+
+use NorthStack\NorthStackClient\API\BaseApiClient;
+
+class NorthstackClient extends BaseApiClient
+{
+    protected $apiName = 'northstack';
+
+    public function launchWorker(
+        $accessToken,
+        $sappId
+    )
+    {
+        return $this->guzzle($this->getBearerTokenMiddleware($accessToken))
+            ->post("/northstack/worker/launch/$sappId");
+    }
+}
---
+++ src/Command/Sapp/LaunchCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+
+namespace NorthStack\NorthStackClient\Command\Sapp;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
+use NorthStack\NorthStackClient\API\AuthApi;
+use NorthStack\NorthStackClient\API\Northstack\NorthstackClient;
+
+use NorthStack\NorthStackClient\Command\Command;
+use NorthStack\NorthStackClient\Command\OauthCommandTrait;
+
+class LaunchCommand extends Command
+{
+    use OauthCommandTrait;
+    use SappEnvironmentTrait;
+    /**
+     * @var NorthstackClient
+     */
+    protected $api;
+    /**
+     * @var Client
+     */
+    private $guzzle;
+
+    public function __construct(
+        NorthstackClient $api,
+        AuthApi $authApi,
+        Client $guzzle
+    )
+    {
+        parent::__construct('app:debug:launch-worker');
+        $this->api = $api;
+        $this->authClient = $authApi;
+        $this->guzzle = $guzzle;
+    }
+
+    public function configure()
+    {
+        parent::configure();
+        $this
+            ->setDescription('Launch a worker for an App')
+            ->setHelp('Northstack will launch additional workers for you as needed.  This command can be used for debugging, or to prime additional capacity.  Note that unused capacity will shutoff after the inactivity period.')
+            ->addArgument('name', InputArgument::REQUIRED, 'App name')
+            ->addArgument('environment', InputArgument::REQUIRED, 'Environment (prod, test, or dev)')
+            ->addArgument('baseFolder', InputArgument::OPTIONAL, 'Path to root of NorthStack folder (contains folder named after app)')
+        ;
+        $this->addOauthOptions();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($output->isDebug())
+        {
+            $this->api->setDebug(true);
+        }
+
+        $args = $input->getArguments();
+
+        if (empty($args['baseFolder']))
+            $args['baseFolder'] = getcwd();
+
+
+        [$sappId, $appFolder] = $this->getSappIdAndFolderByOptions(
+            $args['name'],
+            $args['environment'],
+            $args['baseFolder']
+        );
+
+        try
+        {
+            $r = $this->api->launchWorker(
+                $this->token->token,
+                $sappId
+            );
+        }
+        catch(ClientException $e)
+        {
+            $r = $e->getResponse();
+        }
+
+        $output->writeln("Launch finished code: ".$r->getStatusCode());
+        print_r(json_decode($r->getBody()->getContents()));
+    }
+}
