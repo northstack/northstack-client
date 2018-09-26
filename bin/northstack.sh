4f94fc8c3d348c6d0794a4f228de2cba0951f130
Michael Martin <michael.joe.martin@gmail.com>
2018-09-26 16:31:44 -0700

    Check multiple paths for the docker control socket

--- bin/northstack.sh
+++ bin/northstack.sh
@@ -3,11 +3,22 @@
 # Wrapper for running the northstack cli in docker
 # Handles uid mapping, and given access to the docker sock so northstack can start local dev
 
+
+if [[ -S /var/lib/docker.sock ]]; then
+    socket=/var/lib/docker.sock
+elif [[ -S $HOME/Library/Containers/com.docker.docker/Data/docker.sock ]]; then
+    # the control socket likes to hide here on OSX
+    socket=$HOME/Library/Containers/com.docker.docker/Data/docker.sock
+else
+    echo "Error: no docker control socket found. Is docker installed and running?"
+    exit 1
+fi
+
 docker run -ti --rm \
     --user=$UID --userns=host \
     --volume `pwd`:/current \
     --volume $HOME:$HOME \
     --volume /etc/passwd:/etc/passwd \
-    --volume /var/lib/docker.sock:/var/lib/docker.sock \
+    --volume "$socket":/var/lib/docker.sock \
     --init \
     northstack "$@"
