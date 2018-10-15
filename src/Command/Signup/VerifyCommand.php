bd5ece111e7db7ea7371136ba7560ffc0a8b9a99
Gordon Forsythe <baohx2000@gmail.com>
2018-10-15 11:06:45 -0700

    use org account helper for verify command

--- src/Command/Signup/VerifyCommand.php
+++ src/Command/Signup/VerifyCommand.php
@@ -7,8 +7,10 @@ namespace NorthStack\NorthStackClient\Command\Signup;
 use NorthStack\NorthStackClient\API\Orgs\OrgsClient;
 use NorthStack\NorthStackClient\Command\Command;
 use NorthStack\NorthStackClient\Command\OauthCommandTrait;
+use NorthStack\NorthStackClient\OrgAccountHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class VerifyCommand extends Command
@@ -18,25 +20,34 @@ class VerifyCommand extends Command
      * @var OrgsClient
      */
     protected $api;
+    /**
+     * @var OrgAccountHelper
+     */
+    private $orgAccountHelper;
 
-    public function __construct(OrgsClient $api)
+    public function __construct(OrgsClient $api, OrgAccountHelper $orgAccountHelper)
     {
         parent::__construct('signup:verify');
         $this->api = $api;
+        $this->orgAccountHelper = $orgAccountHelper;
     }
 
     public function configure()
     {
         parent::configure();
         $this->setDescription('NorthStack Signup Phone Verification')
-            ->addArgument('orgId', InputArgument::REQUIRED, 'Organization ID from signup')
-            ->addArgument('code', InputArgument::REQUIRED, 'Code sent via text message');
+            ->addArgument('code', InputArgument::REQUIRED, 'Code sent via text message')
+            ->addOption('orgId', null, InputOption::VALUE_REQUIRED, 'Org ID (if you have access to multiple orgs)')
+        ;
         $this->addOauthOptions();
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->api->verify($this->token->token, $input->getArgument('orgId'), $input->getArgument('code'));
+        if (!($orgId = $input->getOption('orgId'))) {
+            $orgId = $this->orgAccountHelper->getDefaultOrg()['id'];
+        }
+        $this->api->verify($this->token->token, $orgId, $input->getArgument('code'));
 
         $output->writeln('Verified!');
     }
