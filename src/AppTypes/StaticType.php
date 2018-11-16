268ca163ee92ca9a16eb28f708858fc456f1d41b
mm-pagely <michael.martin@pagely.com>
2018-11-16 14:19:49 -0800

    static app compat stuff

--- src/AppTypes/StaticType.php
+++ src/AppTypes/StaticType.php
@@ -6,4 +6,22 @@ use NorthStack\NorthStackClient\AppTypes\BaseType;
 
 class StaticType extends BaseType
 {
+    protected function writePerEnvBuildConfigs()
+    {
+        $this->writeConfigFile(
+            'config/build.json',
+            [
+                'build-type' => 'builder'
+            ]
+        );
+
+        $this->writeConfigFile(
+          'config/config.json',
+          [
+            'app-type' => 'static',
+            'layout' => 'standard',
+            'shared-paths' => ['/']
+          ]
+        );
+    }
 }
