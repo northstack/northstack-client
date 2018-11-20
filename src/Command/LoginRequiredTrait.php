6e3bde086e0147a62a7d12cfff779aac7e371d4d
mm-pagely <michael.martin@pagely.com>
2018-11-20 15:08:13 -0800

    Refactor and add flag to allow commands to skp auth check

--- src/Command/LoginRequiredTrait.php
+++ src/Command/LoginRequiredTrait.php
@@ -10,20 +10,26 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 trait LoginRequiredTrait
 {
+    protected $skipLoginCheck = false;
 
     public function run(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->isInteractive() && !$this->skipLoginCheck) {
+            $this->checkLogin($output);
+        }
+        parent::run($input, $output);
+    }
+
+    protected function checkLogin(OutputInterface $output)
     {
         $auth = $this->getApplication()->find('auth:whoami');
         try {
             $result = $auth->run(new ArrayInput([]), new NullOutput());
         } catch (LoginRequiredException $e) {
-            if ($input->isInteractive()) {
-                $output->writeln("No active NorthStack token found. Please log in.");
-                $login = $this->getApplication()->find('auth:login');
-                $login->run(new ArrayInput([]), $output);
-            }
+            $output->writeln("No active NorthStack token found. Please log in.");
+            $login = $this->getApplication()->find('auth:login');
+            $login->run(new ArrayInput([]), $output);
         }
 
-        parent::run($input, $output);
     }
 }
