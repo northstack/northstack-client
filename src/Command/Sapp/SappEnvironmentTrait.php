a62850d08034702296db4091ed6c15ffc0087474
Michael Martin <michael.martin@pagely.com>
2018-12-04 22:18:24 -0800

    Load sapp from PWD

--- src/Command/Sapp/SappEnvironmentTrait.php
+++ src/Command/Sapp/SappEnvironmentTrait.php
@@ -4,6 +4,8 @@
 namespace NorthStack\NorthStackClient\Command\Sapp;
 
 
+use NorthStack\NorthStackClient\JSON\Merger;
+
 trait SappEnvironmentTrait
 {
     protected function getSappIdAndFolderByOptions($name, $environment)
@@ -18,5 +20,59 @@ trait SappEnvironmentTrait
         $envConfig = json_decode(file_get_contents("{$appFolder}/config/environment.json"), true);
 
         return [$envConfig[$environment], $appFolder];
+    }
+
+    protected function getSappFromWorkingDir($env = 'prod')
+    {
+        $cwd = getcwd();
+        $name = basename($cwd);
+
+        array_map(
+            function($path) use ($cwd)
+            {
+                $path = $cwd . $path;
+                if (!file_exists($path))
+                {
+                    throw new \Exception("Command must be executed inside an app directory (missing: {$path})");
+                }
+            },
+            [
+                '/app/public',
+                '/config/build.json',
+                '/config/config.json',
+                '/config/environment.json',
+                "/config/{$env}"
+            ]
+        );
+
+        $conf = $this->mergeConfigs($cwd, $env, true);
+        $environments = json_decode(file_get_contents("{$cwd}/config/environment.json"));
+        $conf['name'] = $name;
+        $conf['id'] = $environments->{$env};
+        $conf['env'] = $env;
+        return $conf;
+    }
+
+    protected function mergeConfigs(string $appFolder, string $environment, $decode = false)
+    {
+        $configs = [
+            'config' => file_get_contents("{$appFolder}/config/config.json"),
+            'build' => file_get_contents("{$appFolder}/config/build.json"),
+            'domains' => '{}',
+        ];
+        foreach ($configs as $file => $json) {
+            $envFile = "{$appFolder}/config/{$environment}/{$file}.json";
+            if (file_exists($envFile)) {
+                $configs[$file] = Merger::merge($json, file_get_contents($envFile));
+            } else {
+                $configs[$file] = Merger::merge($json, '{}');
+            }
+
+            if ($decode)
+            {
+                $configs[$file] = json_decode($configs[$file]);
+            }
+        }
+        return $configs;
     }
 }
