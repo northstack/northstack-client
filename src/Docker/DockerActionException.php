53deb5c65f6d3523ea8f7ab9a6a7de1c04e7492b
Michael Martin <michael.martin@pagely.com>
2018-12-10 10:25:38 -0800

    exception handling stuff

--- src/Docker/DockerActionException.php
+++ src/Docker/DockerActionException.php
@@ -2,13 +2,18 @@
 
 namespace NorthStack\NorthStackClient\Docker;
 
+use Docker\API\Exception\ApiException;
 use Docker\API\Model\ErrorResponse;
 
 class DockerActionException extends \Exception
 {
     public static function fromException(\Exception $e)
     {
-        if (method_exists($e, 'getErrorResponse')
+        // All docker-php exceptions use the ApiException interface, but
+        // getErrorResponse() is not part of the interface, so we have to
+        // check by hand :(
+        if ($e instanceof ApiException
+            && method_exists($e, 'getErrorResponse')
             && $e->getErrorResponse() instanceof ErrorResponse)
         {
             $msg = $e->getErrorResponse()->getMessage();
