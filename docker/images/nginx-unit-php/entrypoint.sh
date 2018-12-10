fe07bda4db68b72bbfb1c2bba7dfe2199ece315a
Michael Martin <michael.martin@pagely.com>
2018-12-10 13:28:18 -0800

    Add user for nginx-unit in entrypoint

--- docker/images/nginx-unit-php/entrypoint.sh
+++ docker/images/nginx-unit-php/entrypoint.sh
@@ -2,6 +2,18 @@
 
 set -eu
 
+groupadd \
+    --force \
+    --non-unique \
+    --gid "$NORTHSTACK_GID" \
+    "$NORTHSTACK_GROUP"
+
+useradd \
+    --uid "$NORTHSTACK_UID" \
+    --no-create-home \
+    --gid "$NORTHSTACK_GID" \
+    "$NORTHSTACK_USER"
+
 sed \
     -e "s/NORTHSTACK_USERNAME/$NORTHSTACK_USER/g" \
     -e "s/NORTHSTACK_GROUP/$NORTHSTACK_GROUP/g" \
