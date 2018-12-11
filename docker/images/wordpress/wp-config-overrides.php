13b1877eb16de07fc9d11a78a5c620164c005b19
Michael Martin <michael.martin@pagely.com>
2018-12-10 18:40:47 -0800

    handle ports in the host header

--- docker/images/wordpress/wp-config-overrides.php
+++ docker/images/wordpress/wp-config-overrides.php
@@ -4,6 +4,7 @@ if (getenv('EXPOSE_HTTP_PORT'))
 {
     $scheme = $_SERVER['HTTPS_ON'] ? 'https' : 'http';
     $host = $_SERVER['HTTP_HOST'] ?: 'localhost';
+    [$host, $_] = explode(':', $host, 2);
     $port = getenv('EXPOSE_HTTP_PORT');
 
     $url = "{$scheme}://${host}:{$port}";
