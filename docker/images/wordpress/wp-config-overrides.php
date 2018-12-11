26ad7386baacc7d52f97bc96965e1c0619aef3e7
Michael Martin <michael.martin@pagely.com>
2018-12-10 18:25:18 -0800

    don't need that error_log anymore

--- docker/images/wordpress/wp-config-overrides.php
+++ docker/images/wordpress/wp-config-overrides.php
@@ -2,8 +2,6 @@
 
 if (getenv('EXPOSE_HTTP_PORT'))
 {
-    error_log("Overriding home/siteurl");
-
     $scheme = $_SERVER['HTTPS_ON'] ? 'https' : 'http';
     $host = $_SERVER['HTTP_HOST'] ?: 'localhost';
     $port = getenv('EXPOSE_HTTP_PORT');
