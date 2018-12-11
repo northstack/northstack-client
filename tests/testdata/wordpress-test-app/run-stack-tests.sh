87801db8dd2bb4c70e910e855e05f23116b751a0
Michael Martin <michael.martin@pagely.com>
2018-12-10 16:48:36 -0800

    testing WIP

--- tests/testdata/wordpress-test-app/run-stack-tests.sh
+++ tests/testdata/wordpress-test-app/run-stack-tests.sh
@@ -1,3 +1,59 @@
 #!/usr/bin/env bash
 
-docker-compose ps
+set -eu
+
+function awaitFile() {
+    local file=$1
+    local maxwait=${2:-20}
+
+    local tries=0
+    while [[ ! -e $file ]] && (( tries < maxwait )); do
+        echo "Waiting for $file to exist"
+        sleep 1
+        tries=$((tries++))
+    done
+
+    if [[ ! -e $file ]]; then
+        echo "timeout reached waiting for $file"
+        return 1
+    fi
+}
+
+function checkHttp() {
+    local uri=$1
+    local status=${2:-200}
+
+    local req="http://localhost:8080${uri}"
+    local ret=$(curl -s -o /dev/null -w '%{http_code}' "$req")
+
+    if [[ $? -ne 0 ]]; then
+        echo "curl returned nonzero: $?"
+        return $?
+    fi
+
+    echo "Checked: $req"
+    echo "Expected: $status"
+    echo "Returned: $ret"
+    [[ $ret == $status ]] && echo success
+}
+
+awaitFile $PWD/app/public/index.php
+
+echo can we run docker-compose
+{
+    docker-compose ps
+}
+
+#echo can we run a wp-cli command
+#{
+#    cd app/public
+#    wp plugin status
+#    cd -
+#}
+
+echo is wordpress up and running?
+{
+    checkHttp /
+    checkHttp /wp-login.php
+}
+
