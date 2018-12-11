6675799f0dc1d5d48b91c7c02d7b3d20ac1ef427
Michael Martin <michael.martin@pagely.com>
2018-12-10 21:23:55 -0800

    testing for all

--- tests/test.sh
+++ tests/test.sh
@@ -7,6 +7,28 @@ cd $BDIR
 
 export DEBUG=1
 source "$BDIR/bin/lib.sh"
-buildDockerImage . northstack-test
+buildDockerImage .
+docker build -t northstack-test -f Dockerfile-test .
 
-docker run --rm --entrypoint /app/tests/run-all.sh northstack-test
+NS_PWD="$BDIR"
+
+docker run \
+    --rm \
+    -it \
+    -e DEBUG=$DEBUG \
+    -e HOME=$HOME \
+    -e NS_PWD="$NS_PWD" \
+    -e NS_LIB="$BDIR" \
+    -e PAUSE=$PAUSE \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --volume "$NS_PWD:$NS_PWD" \
+    --volume "$HOME:$HOME" \
+    --volume "$BDIR:/app" \
+    --entrypoint /app/tests/run-all.sh \
+    --user=$UID:$(id -g) \
+    --volume "/etc/passwd:/etc/passwd:ro" \
+    --volume "/etc/group:/etc/group:ro" \
+    --group-add="$(stat /var/run/docker.sock --printf='%G')" \
+    --init \
+    northstack-test
+
