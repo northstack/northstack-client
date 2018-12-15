5d6d37153fc0645d882dde351bf211ceedbf4c2d
Michael Martin <michael.martin@pagely.com>
2018-12-15 09:24:14 -0800

    better useradd/groupadd logic

--- docker/images/nginx-unit-php/entrypoint.sh
+++ docker/images/nginx-unit-php/entrypoint.sh
@@ -2,26 +2,73 @@
 
 set -eu
 
+localIdExists() {
+    local id=$1
+    local ns=$2
 
-if getent group "$NORTHSTACK_GID" > /dev/null; then
-    grp=$(getent group "$NORTHSTACK_GID" | cut -d: -f 1)
-    echo "Remvoing duplicate group $grp with gid $NORTHSTACK_GID"
-    groupdel -f "$grp"
-fi
+    getent "$ns" "$id" &> /dev/null
+    return $?
+}
 
-echo "Adding group $NORTHSTACK_GROUP"
-groupadd \
-    --force \
-    --gid "$NORTHSTACK_GID" \
-    "$NORTHSTACK_GROUP"
+getLocalName() {
+    local id=$1
+    local ns=$2
+    getent "$ns" "$id" | cut -d: -f 1
+}
 
-echo "Adding user $NORTHSTACK_USER"
-useradd \
-    --uid "$NORTHSTACK_UID" \
-    --no-create-home \
-    --key MAIL_DIR=/tmp \
-    --gid "$NORTHSTACK_GID" \
-    "$NORTHSTACK_USER"
+deleteLocal() {
+    local name=$1
+    local ns=$2
+
+    if [[ $ns == "group" ]]; then
+        groupdel -f "$name"
+    elif [[ $ns == "passwd" ]]; then
+        userdel "$name"
+    fi
+}
+
+addLocal() {
+    local name=$1
+    local id=$2
+    local ns=$3
+    local extra=${4:-}
+
+    if [[ $ns == "group" ]]; then
+        groupadd \
+            --force \
+            --gid "$id" \
+            "$name"
+    elif [[ $ns == "passwd" ]]; then
+        useradd \
+            --uid "$id" \
+            --no-create-home \
+            --key MAIL_DIR=/tmp \
+            --gid "$extra" \
+            "$name"
+    fi
+}
+
+addIfNotExists() {
+    local name=$1
+    local id=$2
+    local ns=$3
+    local extra=${4:-}
+
+    if localIdExists "$id" "$ns"; then
+        local current=$(getLocalName "$id" "$ns")
+        if [[ $name == $current ]]; then
+            # desired entity already exists
+            return 0
+        fi
+        # we have another entity using our desired id
+        deleteLocal "$current" "$ns"
+    fi
+
+    addLocal "$name" "$id" "$ns" "$extra"
+}
+
+addIfNotExists "$NORTHSTACK_GROUP" "$NORTHSTACK_GID" group
+addIfNotExists "$NORTHSTACK_USER" "$NORTHSTACK_UID" passwd "$NORTHSTACK_GID"
 
 sed \
     -e "s/NORTHSTACK_USERNAME/$NORTHSTACK_USER/g" \
