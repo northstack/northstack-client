4faeeb1e0da4d56c1e30387b0a67686a4278166c
Gordon Forsythe <baohx2000@gmail.com>
2019-03-21 13:30:49 -0700

    debug

--- src/Docker/Builder/RubyBuilder.php
+++ src/Docker/Builder/RubyBuilder.php
@@ -10,7 +10,7 @@ class RubyBuilder extends AbstractBuilder implements BuilderInterface
 
     public function run()
     {
-        $this->exec('ls -l /');
+        $this->exec('ls -l /scripts');
         $this->exec("ruby /scripts/{$this->config->getScript()}");
     }
 }
