38f2c6de56b1ee46ce047e104dd28b9d955edf31
Maura Teal <maura@mlteal.com>
2019-05-14 15:01:16 -0700

    add some BK 👑 sections to tests

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -5,8 +5,10 @@ CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BDIR=$(dirname $CDIR)
 cd $BDIR
 
+echo "--- :php: :clippy: PHPStan"
 ./vendor/bin/phpstan analyze --level 0 src
 
+echo "--- :php: :parrot: Running PHPUnit"
 ./vendor/bin/phpunit tests
 
 if [[ ${TEST_LOCALDEV:-0} == 1 ]]; then
