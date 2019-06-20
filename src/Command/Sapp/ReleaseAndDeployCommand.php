a60acf0dd2bdfa2100261b376184f19e06bc02a5
Gordon Forsythe <baohx2000@gmail.com>
2019-06-20 10:54:25 -0700

    inject config entities

--- src/Command/Sapp/ReleaseAndDeployCommand.php
+++ src/Command/Sapp/ReleaseAndDeployCommand.php
@@ -49,7 +49,7 @@ class ReleaseAndDeployCommand extends AbstractDeployCmd
         $environment = $input->getArgument('environment');
         $configs = $this->mergeConfigs($appFolder, $environment);
 
-        $appType = json_decode($configs['config'])->app_type;
+        $appType = $configs['configObject']->app_type;
 
         // update configs
         $output->writeln('Updating Sapp configs...');
