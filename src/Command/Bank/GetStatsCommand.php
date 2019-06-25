3c310fa0a40250f4c85e0dbb06870369f2c32d7e
Joshua Eichorn <joshua.eichorn@pagely.com>
2019-06-25 22:53:17 +0000

    Output

--- src/Command/Bank/GetStatsCommand.php
+++ src/Command/Bank/GetStatsCommand.php
@@ -6,11 +6,15 @@ use NorthStack\NorthStackClient\API\Bank\BankClient;
 use NorthStack\NorthStackClient\Command\Command;
 use NorthStack\NorthStackClient\Command\OauthCommandTrait;
 
+use NorthStack\NorthStackClient\OrgAccountHelper;
+use NorthStack\NorthStackClient\API\Orgs\OrgsClient;
+
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Helper\Table;
 
 class GetStatsCommand extends Command
 {
@@ -20,25 +24,48 @@ class GetStatsCommand extends Command
      */
     protected $api;
 
+    /**
+     * @var OrgsClient
+     */
+    private $orgs;
+
+
+    /**
+     * @var OrgAccountHelper
+     */
+    protected $orgAccountHelper;
+
     public function __construct(
-        BankClient $api
+        BankClient $api,
+        OrgsClient $orgs,
+        OrgAccountHelper $orgAccountHelper
     )
     {
         parent::__construct('stats:get');
         $this->api = $api;
+        $this->orgs = $orgs;
+        $this->orgAccountHelper = $orgAccountHelper;
     }
 
     public function configure()
     {
+        $OPTION_ARRAY = InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED;
         parent::configure();
         $this
             ->setDescription('Get Stats for an App')
-            ->addArgument('org', InputArgument::REQUIRED, 'Org ID')
-            ->addArgument('appids', InputArgument::REQUIRED, 'App ids comma separated')
-            ->addArgument('type', InputArgument::REQUIRED, 'Stats Type (access, worker)')
-            ->addOption('from', null, InputOption::VALUE_REQUIRED, 'From date YYYY-MM-DD', date('Y-m-d', strtotime('-30 days')))
-            ->addOption('to', null, InputOption::VALUE_REQUIRED, 'To date YYYY-MM-DD', date('Y-m-d'))
-            ->addOption('field-set', null, InputOption::VALUE_REQUIRED, 'Which fields to display: basic, all, percentile', 'basic')
+            ->addArgument('type', InputArgument::REQUIRED, 'Stats Type [traffic]')
+            ->addOption('from', null, InputOption::VALUE_REQUIRED, 'From date, any format recognizable by strtotime', '-1 hour')
+            ->addOption('to', null, InputOption::VALUE_REQUIRED, 'To date, any format recognizable by strtotime', 'now')
+            ->addOption('window', null, InputOption::VALUE_REQUIRED, 'Aggregate data into this interval (minutes)', '1')
+            ->addOption('field-set', null, InputOption::VALUE_REQUIRED, 'all, percentile, basic', 'basic')
+            ->addOption('orgId', null, InputOption::VALUE_REQUIRED, 'Org ID')
+            ->addOption('appId', null, $OPTION_ARRAY, 'App id')
+            ->addOption('httpCode', null, $OPTION_ARRAY, 'Http codes (200,404,etc)')
+            ->addOption('method', null, $OPTION_ARRAY, 'Http method, (GET, POST,etc)')
+            ->addOption('cacheStatus', null, $OPTION_ARRAY, 'Cache Status, (HIT, MISS, BYPASS)')
+            ->addOption('scheme', null, $OPTION_ARRAY, 'http scheme, (http, https)')
+            ->addOption('host', null, $OPTION_ARRAY, 'hostname, (example.com, www.example.com)')
+            ->addOption('contentType', null, $OPTION_ARRAY, 'Content Type, (assets,content)')
         ;
         $this->addOauthOptions();
     }
@@ -51,59 +78,79 @@ class GetStatsCommand extends Command
         }
 
         $args = $input->getArguments();
-        $options = $input->getOptions();
+        $orgId = $input->getOption('orgId') ?: $this->orgAccountHelper->getDefaultOrg()['id'];
+        $user = $this->requireLogin($this->orgs);
 
-        $io = new SymfonyStyle($input, $output);
+        $filters = [
+            'fromDate' => $input->getOption('from'),
+            'toDate' => $input->getOption('to'),
+        ];
 
-        $r = $this->api->statsForApp(
+        $map = [
+            'appId' => 'sappIds',
+            'window' => 'window',
+            'scheme' => 'scheme',
+            'method' => 'method',
+            'host' => 'host',
+            'contentType' => 'type',
+            'httpCode' => 'httpCode',
+        ];
+
+        foreach($map as $option => $filter) {
+            $optionValue = $input->getOption($option);
+            if ($optionValue !== false && (!is_array($optionValue) || count($optionValue) > 1)) {
+                $filters[$filter] = $input->getOption($option);
+            }
+        }
+
+        $r = $this->api->statsForOrg(
             $this->token->token,
-            $args['org'],
-            explode(',', $args['appids']),
-            [$args['type']],
-            $options['to'],
-            $options['from']
+            $orgId,
+            $args['type'],
+            $filters
         );
         $stats = json_decode($r->getBody()->getContents(), true);
 
-        $fieldSet['all'] = array_keys($stats['data'][0]);
-        $fieldSet['basic'] = [
-            'time',
-            'type',
-            'scheme',
-            'method',
-            'httpCode',
-            'cacheStatus',
-            'count',
-            'average',
-            'median',
-            'max',
-            'min',
-        ];
-        $fieldSet['percentile'] = [
-            'time',
-            'type',
-            'scheme',
-            'method',
-            'httpCode',
-            'cacheStatus',
-            'count',
-            'p05',
-            'p25',
-            'p50',
-            'p75',
-            'p95',
-        ];
-
-        $headers = $fieldSet[$options['field-set']];
-        $rows = [];
-        foreach($stats['data'] as $row) {
-            $r = [];
-            foreach($fieldSet[$options['field-set']] as $key) {
-                $r[] = $row[$key];
-            }
-            $rows[] = $r;
+        $skip = [];
+        switch($input->getOption('field-set')) {
+        case 'basic':
+            $skip = ['p05','p25','p50','p75','p95'];
+            break;
+        case 'percentile':
+            $skip = ['max','min','total','average','median'];
+            break;
         }
 
-        $io->table($headers, $rows);
+        foreach($stats['data']['series'] as $series) {
+            $table = new Table($output);
+
+            $columns = $series['columns'];
+            foreach($skip as $i) {
+                $toDel = array_search($i,$columns);
+                if ($toDel !== false) {
+                    unset($columns[$toDel]);
+                }
+            }
+            $table->setHeaders($columns);
+
+            $rows = $series['values'];
+            if (count($skip) > 0) {
+                foreach($rows as $i => $row) {
+                    foreach($row as $k => $v) {
+                        if (!isset($columns[$k])) {
+                            unset($row[$k]);
+                        }
+                    }
+                    $rows[$i] = $row;
+                }
+            }
+            $table->setRows($rows);
+            $title = [];
+            foreach($series['tags'] as $k => $v) {
+                $title[] = "$k = $v";
+            }
+            $table->setHeaderTitle(implode(', ',$title));
+            $table->render();
+        }
     }
 }
