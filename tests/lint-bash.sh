c48575df2d13c3b8afb1824558bd4f6c52385db8
Michael Martin <michael.martin@pagely.com>
2019-06-25 15:36:34 -0700

    break build/lint/test into separate steps

---
+++ tests/lint-bash.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eu
+
+CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE="$( dirname "$CDIR")"
+
+BIN_DIR="$BASE/bin"
+REPO=northstack/bats
+
+failed=0
+echo "+++ Linting bash files"
+for f in "$BIN_DIR"/*.sh; do
+    if [[ -L $f ]]; then
+        continue
+    fi
+    f=$(basename "$f")
+    echo "--- Linting: $f"
+    docker run \
+        --rm \
+        --init \
+        --volume "${BASE}:${BASE}:ro" \
+        --workdir "$BIN_DIR" \
+        -e BIN_DIR="$BIN_DIR" \
+        --label com.northstack=1 \
+        --entrypoint /bin/shellcheck \
+        "$REPO:latest" \
+        "$f" \
+    || {
+        echo "^^^ +++"
+        echo Lint errors for $f
+        failed=1
+    }
+done
+
+exit $failed
--- tests/test-bash.sh
+++ tests/test-bash.sh
@@ -6,26 +6,10 @@ CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE="$( dirname "$CDIR")"
 
 BIN_DIR="$BASE/bin"
-
-stamp=$(stat -c %Y "$CDIR"/docker/Dockerfile)
-bats_version=$(awk -F= '/^ARG BATS_VERSION/ {print $2}' "$CDIR"/docker/Dockerfile)
-
-repo=northstack/bats
-current=${repo}:${bats_version}
-
-check=$(docker image ls "$current" --filter label=com.northstack.image.stamp="$stamp" -q)
-if [[ -z $check ]]; then
-    echo "--- Building northstack/bats docker image"
-    docker build \
-        --file "$CDIR/docker/Dockerfile" \
-        --tag "$current" \
-        --tag "${repo}:latest" \
-        --label com.northstack=1 \
-        --label com.northstack.image.stamp="$stamp" \
-        "$CDIR/docker"
-fi
+REPO=northstack/bats
 
 echo "+++ Running bash tests"
+
 if [[ $# -eq 0 ]]; then
     set -- bash
 fi
@@ -38,26 +22,5 @@ docker run \
     -e BIN_DIR="$BIN_DIR" \
     -e DEBUG="${DEBUG:-}" \
     --label com.northstack=1 \
-    "$repo":latest \
+    "$REPO:latest" \
     "$@"
-
-
-echo "+++ Linting bash files"
-files=()
-for f in "$BIN_DIR"/*.sh; do
-    if [[ -f $f ]]; then
-        f=$(basename "$f")
-        files+=("$f")
-    fi
-done
-
-docker run \
-    --rm \
-    --init \
-    --volume "${BASE}:${BASE}:ro" \
-    --workdir "$BIN_DIR" \
-    -e BIN_DIR="$BIN_DIR" \
-    --label com.northstack=1 \
-    --entrypoint /bin/shellcheck \
-    "$repo":latest \
-    ${files[*]}
