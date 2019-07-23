8379d3ae8589f1762d73a4289070685381ed1f82
Joshua Eichorn <joshua.eichorn@pagely.com>
2019-07-23 21:52:25 +0000

    new log streaming api, with timeout support

--- src/API/Logs/LogsClient.php
+++ src/API/Logs/LogsClient.php
@@ -16,7 +16,11 @@ class LogsClient extends BaseApiClient
     protected $sender;
     protected $topic;
     protected $accessToken;
+    protected $timeout;
 
+    /**
+     * @deprecated see streamLog
+     */
     public function streamTopic(string $accessToken, callable $sender, array $topic, OutputInterface $output = null)
     {
         $this->sender = $sender;
@@ -57,5 +61,60 @@ class LogsClient extends BaseApiClient
     {
         return $this->guzzle($this->getBearerTokenMiddleware($accessToken))
             ->get("sapps/{$sappId}/build-logs");
+    }
+
+    /**
+     * @var $timeout in seconds, 0 means no timeout
+     */
+    public function streamLog(string $accessToken, callable $sender, array $topic, int $timeout = 0, OutputInterface $output = null)
+    {
+        $this->topic = $topic;
+        $this->sender = $sender;
+        $this->accessToken = $accessToken;
+        $this->timeout = $timeout;
+        $output = function($msg) use($output) {
+            if ($output !== null) {
+                $output->writeln($msg);
+            }
+        };
+
+        $uri = str_replace('http', 'ws', $this->baseUrl);
+        $uri = rtrim($uri, '/');
+        $uri .= '/logs';
+
+        $loop = \React\EventLoop\Factory::create();
+        $reactConnector = new \React\Socket\Connector($loop);
+        $connector = new \Ratchet\Client\Connector($loop, $reactConnector);
+
+        $connector($uri)
+            ->then(function(Ratchet\Client\WebSocket $conn) use ($loop, $output) {
+
+                if ($this->timeout > 0) {
+                    $loop->addTimer(2, function() use($conn) {
+                        $output("Reached timeout");
+                        $conn->close();
+                    });
+                }
+
+                $conn->on('message', function(\Ratchet\RFC6455\Messaging\MessageInterface $msg) use ($conn) {
+                    $sender = $this->sender;
+                    $sender($msg);
+                });
+
+                $conn->on('close', function($code = null, $reason = null) {
+                    $output("Connection closed ({$code} - {$reason})");
+                });
+
+                $conn->send(json_encode([
+                    'accessToken' => $this->accessToken,
+                    'action' => 'subscribe',
+                    'topic' => $this->topic,
+                ]));
+            }, function(\Exception $e) use ($loop) {
+                $loop->stop();
+                throw new \Exception("Failed to connect: {$e->getMessage()}");
+            });
+
+        $loop->run();
     }
 }
